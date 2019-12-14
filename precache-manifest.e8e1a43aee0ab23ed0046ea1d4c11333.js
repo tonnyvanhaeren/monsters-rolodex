9e1e3de5e335cf8e658c3c497c7eebaa79da0936
Antonius Vanhaeren <antonius.vanhaeren.av@gmail.com>
2019-12-14 16:57:02 +0100

    Updates

---
+++ precache-manifest.e8e1a43aee0ab23ed0046ea1d4c11333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf830b7f1c7b1e57058b216bdc2c1d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2ff65eb6ea9562c1dac",
+    "url": "/monsters-rolodex/static/css/main.4db3edea.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2ff65eb6ea9562c1dac",
+    "url": "/monsters-rolodex/static/js/main.1441835a.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
